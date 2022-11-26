ed576be512ab3ae57f1bbb66e325fa72365170a0
Franklin ospino <desarrollo@expertosip.com>
2022-11-26 11:21:26 -0500

    antes de actualizar material

--- src/app/modules/crud/components/table-material-layout/table-material-layout.component.ts
+++ src/app/modules/crud/components/table-material-layout/table-material-layout.component.ts
@@ -1,5 +1,7 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 import { AfterViewInit, Component, Injectable, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator, MatPaginatorIntl} from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import {Subject} from 'rxjs';
 import { ColumnTableMaterialLayout } from '../../interface/table';
@@ -54,13 +56,25 @@ export class TableMaterialLayoutComponent implements OnInit, AfterViewInit {
     }
   ];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-  ngOnInit(){
+  @ViewChild(MatSort) sort: MatSort;
 
-  }
+  constructor(private _liveAnnouncer: LiveAnnouncer) { }
+  ngOnInit(){ }
   ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+  }
+
+  /**
+   * TABLE METHODS
+   */
+   announceSortChange(sortState: any) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
   /**
